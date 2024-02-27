b8aa34ad5b6b7958e76bc5e6ebec0f30d7aeac6a
Joharisoa RATSIRARSON <joharisoa.nexthope@gmail.com>
2024-02-27 21:25:29 +0300

    Liste - Ajout - Rendre Devoir

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -2,38 +2,47 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RenduDirective } from '../shared/rendu.directive';
 
+import { AssignmentDetailComponent } from './assignment-detail/assignment-detail.component';
+
 
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+
+
 import { OnInit } from '@angular/core';
 import { Assignment } from './assignment.model';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-assignments',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [CommonModule, RenduDirective,
     FormsModule,
     MatButtonModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule,
     MatFormFieldModule,
-    MatIconModule,],
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    AssignmentDetailComponent],
   templateUrl: './assignments.component.html',
   styleUrl: './assignments.component.css'
 })
 export class AssignmentsComponent implements OnInit{
-  titre = 'Liste des assignements (vide pour le moment)';
+  titre = 'Ajour d\'un devoir';
   ajoutActive = false;
   nomDevoir:string = "";
-  datedeRendu:Date = new Date();
+  datedeRendu = undefined;
 
-
+  assignmentSelectionne!:Assignment;
 
 
   ngOnInit(): void {
@@ -43,6 +52,9 @@ export class AssignmentsComponent implements OnInit{
   }
 
   onSubmit(){
+    if(this.nomDevoir == '' || this.datedeRendu == undefined){
+      return;
+    }
     const newAssignment = new Assignment();
     newAssignment.nom = this.nomDevoir;
     newAssignment.datedeRendu = this.datedeRendu;
@@ -71,5 +83,9 @@ export class AssignmentsComponent implements OnInit{
 
   getColor(a:any){
     return a.rendu ? 'green' : 'red';
+  }
+
+  assignmentClique(assignment:Assignment){
+    this.assignmentSelectionne = assignment;
   }
 }
